cc1794278c3f9e940e7c519b79f5047781bbf67a
Tim Reynolds <tmrdev@gmail.com>
2021-07-01 22:36:26 -0400

    remove dev tools

--- src/background.js
+++ src/background.js
@@ -30,7 +30,7 @@ async function createWindow() {
     width: width,
     height: height,
     webPreferences: {
-      devTools: true,
+      devTools: false,
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
